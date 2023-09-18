178cf7a681f69ba8b733765044ae8970360c51fc
Moshiach770 <109598704+samWW2@users.noreply.github.com>
2023-09-18 14:26:36 +0300

    setup ngrok and added ngrok base url

--- movie-api-front/src/api/axiosConfig.js
+++ movie-api-front/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
+    baseURL:'https://f0ab-176-228-131-173.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
